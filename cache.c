9e6aa5acff19ef70427bcda70334ffa0e4007fff
Park Kyung Won <park0kyung0won@dgist.ac.kr>
2017-06-18 16:34:23 +0000

    Cache module has been constructed

--- cache.c
+++ cache.c
@@ -1,9 +1,218 @@
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include "cache.h"
+#include <math.h>
 
-bool cache(int addr)
+void get_tag_and_index(struct cons_cache_controller *cache_cont, int *tag_and_index, int addr)
 {
+	int tag = addr >> (*cache_cont).tag_filter_bits;
+	int index = addr >> (*cache_cont).index_filter_bits;
+	index = index % (*cache_cont).set_numbers;
 
-	return true;
+	tag_and_index[0] = tag;
+	tag_and_index[1] = index;
+}
 
+// Declaration of cache_controller
+void update_LRU(struct cons_cache_controller *self, int index, int target_way)
+{
+	int i;
+	for (i = 0; i < (*self).way_numbers; i++)
+	{
+		if ( (*self).LRU[index][i] < (*self).LRU[index][target_way] )
+		{
+			(*self).LRU[index][i]++;
+		}
+	}
+	(*self).LRU[index][target_way] = 0;	
+}
+
+int find_victim(struct cons_cache_controller *self, int index)
+{
+	int victim = 0;
+	int i;
+	for (i = 0; i < (*self).way_numbers; i++)
+	{
+		if ( (*self).LRU[index][i] == ((*self).way_numbers - 1) )
+		{
+			victim = i;
+			break;
+		}
+	}
+
+	return victim;
+}
+
+void cache_fill(struct cons_cache_controller *self, struct cons_cache *cache, struct statistics *stat, int addr)
+{
+	int tag_and_index[2];
+	get_tag_and_index(self, tag_and_index, addr);
+	int tag = tag_and_index[0];
+	int index = tag_and_index[1];
+
+	int target_way = (*self).find_victim(self, index);
+	if ( (*cache).data[index][target_way][2] == 1)
+	{
+		if ( (*cache).data[index][target_way][1] == 1)
+		{
+			(*stat).Dirty_evictions++;
+		}
+		else
+		{
+			(*stat).Clean_evictions++;
+		}
+	}
+
+	// Load desired block from RAM
+	(*cache).data[index][target_way][0] = tag;  // Write new data to the block
+	(*cache).data[index][target_way][1] = 0;    // This is clean block
+	(*cache).data[index][target_way][2] = 1;    // This block is now valid
+	(*self).update_LRU(self, index, target_way);
+}
+
+int search(struct cons_cache_controller *self, struct cons_cache *cache, int addr)
+{
+	// Search whether that block exists or not
+	int target_way = - 1;
+
+	int tag_and_index[2];
+	get_tag_and_index(self, tag_and_index, addr);
+	int tag = tag_and_index[0];
+	int index = tag_and_index[1];
+
+	int way;
+	for (way = 0; way < (*self).way_numbers; way++)
+	{
+		if ( (*cache).data[index][way][0] == tag && (*cache).data[index][way][2] == 1)
+		{
+			target_way = way;
+			break;
+		}
+	}
+
+	return target_way;
+}
+
+bool read_try(struct cons_cache_controller *self, struct cons_cache *cache, struct statistics *stat, int addr)
+{
+	// Try to read from cache[ <addr> ]
+	(*stat).Read_accesses++; // Count read access
+	int tag_and_index[2];
+	get_tag_and_index(self, tag_and_index, addr);
+	int tag = tag_and_index[0];
+	int index = tag_and_index[1];
+
+	int target_way = (*self).search(self, cache, addr);
+	if ( target_way == -1)
+	{
+		(*stat).Read_misses++;
+		return false;
+	}
+	else
+	{
+		(*self).update_LRU(self, index, target_way);
+		return true;
+	}		
+}
+
+bool write_try(struct cons_cache_controller *self, struct cons_cache *cache, struct statistics *stat, int addr)
+{
+	// Try to read from cache[ <addr> ]
+	(*stat).Write_accesses++;
+	int tag_and_index[2];
+	get_tag_and_index(self, tag_and_index, addr);
+	int tag = tag_and_index[0];
+	int index = tag_and_index[1];
+
+	int target_way = (*self).search(self, cache, addr);
+	if ( target_way == -1)
+	{
+		(*stat).Write_misses++;
+		return false;
+	}
+	else
+	{
+		(*cache).data[index][target_way][0] = tag;  // Save tag
+		(*cache).data[index][target_way][1] = 1;    // Dirty bit set
+		(*self).update_LRU(self, index, target_way);
+		return true;
+	}
+}
+
+bool cache_query(struct cons_cache_controller *cache_cont, struct cons_cache *cache, struct statistics *stat, int access_type, int addr)
+{
+	bool result;
+	if (access_type == READ)
+	{
+		result = (*cache_cont).read_try(cache_cont, cache, stat, addr);
+	}
+	else if (access_type == WRITE)
+	{
+		result = (*cache_cont).write_try(cache_cont, cache, stat, addr);
+	}
+	else
+	{
+		printf("Warning! Illegal request to cache controller: %d\n", access_type);
+		result = false;
+	}
+
+	return result;
+}
+
+void cache_filler(struct cons_cache_controller *cache_cont, struct cons_cache *cache, struct statistics *stat, int addr)
+{
+	(*cache_cont).cache_fill(cache_cont, cache, stat, addr);
+}
+
+void *cache_initializer(struct cache_config *config)
+{
+	int capacity = (*config).capacity;
+	int block_size = (*config).block_size;
+	int way_numbers = (*config).way_numbers;
+	int set_numbers = (*config).set_numbers;
+	
+	// Initializing cache_controller *cache_cont 
+	struct cons_cache_controller *cache_cont = malloc(sizeof(struct dummy) + ((sizeof(int) * way_numbers * set_numbers)));
+	// Basic variables initialization
+	(*cache_cont).capacity = capacity;
+	(*cache_cont).block_size = block_size;
+	(*cache_cont).way_numbers = way_numbers;
+	(*cache_cont).set_numbers = set_numbers;
+	
+	// LRU data structure initilization
+	int i, j;
+	for (i = 0; i < set_numbers; i++)
+	{
+		for (j = 0; j < way_numbers; j++)
+		{
+			(*cache_cont).LRU[i][j] = way_numbers - (j + 1);
+		}
+	}
+
+	// Methods initialization
+	(*cache_cont).update_LRU = update_LRU;
+	(*cache_cont).find_victim = find_victim;
+	(*cache_cont).cache_fill = cache_fill;
+	(*cache_cont).search = search;
+	(*cache_cont).read_try = read_try;
+	(*cache_cont).write_try = write_try;
+	
+	// Initializing cache *cache
+	struct cons_cache *cache = calloc((3 * way_numbers * set_numbers), sizeof(int));
+	
+	// Initializing statistics *stat
+	struct statistics *stat;
+	(*stat).Read_accesses = 0;
+	(*stat).Write_accesses = 0;
+	(*stat).Read_misses = 0;
+	(*stat).Write_misses = 0;
+	(*stat).Clean_evictions = 0;
+	(*stat).Dirty_evictions = 0;
+
+	// Returning Declared & Initialized objects
+	void **objects = malloc(sizeof(void *) * 3);
+	objects[0] = cache_cont;
+	objects[1] = cache;
+	objects[2] = stat;
 }
--- cache.h
+++ cache.h
@@ -1,3 +1,128 @@
 #include <stdbool.h>
+/* 캐시 모듈 사용법 
+ * 
+ *
+ * 1. 초기화
+ * 모듈에는 세 가지 핵심적인 클래스가 있다.
+ * 바로 cache_controller (이하 cache_cont)
+ * cache (이하 cache) 그리고
+ * statistics (이하 statistics) 이다.
+ * 이것들을 쓰려면 우선 초기화부터 해야 한다.
+ *
+ * 준비된 cache_config 자료구조의 포인터를 넣어
+ * cache_initializer(struct cache_config *config) 함수를 호출하면
+ * 이 함수는 void형 포인터를 가리키는 포인터를 반환할 것이다.
+ * 사실 이 함수는 void **objects 를 리턴 값으로 가지는데
+ * objects[0] 는 함수 내부에서 malloc으로 생성한 cache_cont의 포인터,
+ * objects[1] 은 함수 내부에서 생성한 cache의 포인터,
+ * objects[2] 는 함수 내부에서 생성한 stat의 포인터를 가지고 있다.
+ *
+ * 그러니 main에서는 cache_initializer를 호출한 뒤에, 어떤 보이드형 포인터로 받고
+ * 이 보이드 형 포인터들을 다시 알맞는 자료형으로 캐스팅하면 생성자가 만든
+ * 객체들에 대한 접근을 할 수 있다.
+ * 
+ * void **objects = cache_initializer(struct cache_config *config);
+ * struct cons_cache_controller *cache_cont = objects[0];
+ * struct cons_cache *cache = objects[1];
+ * struct statistics *stat = objects[2];
+ *
+ *
+ * 2. 캐시 엑세스 (뤼드 / 롸이트)
+ * cache_query 함수를 1번에서 얻은 객체들을 인자로 전달하여 호출하면 
+ * 캐시 접근이 가능하다.
+ * bool cache_query(struct cons_cache controller *cache_cont, struct cons_cache *cache, struct statistics *stat, int access_type, int addr);
+ * 앞의 세 객체는 내부적으로 동작하는데 필요하고 뒤의 두 개 중
+ * access_type은 읽기/쓰기 정보이다. 여기 cache.h에 enum 타입으로 나와 있다.
+ * addr은 접근할 곳의 물리 주소이다.
+ *
+ * 뤼드 혹은 롸이트 접근을 시도하여
+ * 성공하면 true, 실패하면 false의 boolean 값을 반환할 것이다.
+ *
+ *
+ * 3. cache_filler
+ * 이 모듈을 시뮬레이터에 붙이기 위해
+ * 캐시 미스시 캐시 컨트롤러가 핸들링 하는 부분을 없앴다.
+ * 캐시 미스가 나면, Reservation station에 카운트가 50카운트가 되고
+ * 미스가 났던 인스트럭션이 Reservation station에서 타임 카운트가 다 되면
+ * 이 cache_filler 함수를 호출해야 한다.
+ * 이 함수는 메인 메모리에 있는 데이터 블락을 캐시에 써 놓는 역활을 한다.
+ * clock update 관련 사항을 캐시 모듈로 끌어들이기 복잡하니 이 함수를 따로 떼었다.
+ *
+ *
+ */
 
-bool cache(int addr);
+enum { READ = 10, WRITE = 20 };
+
+struct cache_config
+{
+	int capacity;
+	int block_size;
+	int way_numbers;
+	int set_numbers;
+};
+
+struct cons_cache
+{
+	int data[0]; // For struct hack
+};
+
+struct statistics
+{
+	int Read_accesses;
+	int Write_accesses;
+	int Read_misses;
+	int Write_misses;
+	int Clean_evictions;
+	int Dirty_evictions;
+};
+
+struct cons_cache_controller
+{
+	// Methods for cache_controller
+	void (*update_LRU)(struct cons_cache_controller *self, int index, int target_way);
+	int (*find_victim)(struct cons_cache_controller *self, int index);
+	void (*cache_fill)(struct cons_cache_controller *self, struct cons_cache *cache, struct statistics *stat, int addr);
+	int (*search)(struct cons_cache_controller *self, struct cons_cache *cache, int addr);
+	bool (*read_try)(struct cons_cache_controller *self, struct cons_cache *cache, struct statistics *stat, int addr);
+	bool (*write_try)(struct cons_cache_controller *self, struct cons_cache *cache, struct statistics *stat, int addr);
+
+	// Variables for cache_controller
+	// About cache
+	int capacity;
+	int block_size;
+	int way_numbers;
+	int set_numbers;
+	
+	int tag_filter_bits;
+	int index_filter_bits;
+
+	// LRU list for True LRU
+	int LRU[0]; // For struct hat
+};
+
+struct dummy // For struct hat of cons_cache_controller
+{
+	// Methods for cache_controller
+	void (*update_LRU)(struct cons_cache_controller *self, int index, int target_way);
+	int (*find_victim)(struct cons_cache_controller *self, int index);
+	void (*cache_fill)(struct cons_cache_controller *self, struct cons_cache *cache, struct statistics *stat, int addr);
+	int (*search)(struct cons_cache_controller *self, struct cons_cache *cache, int addr);
+	bool (*read_try)(struct cons_cache_controller *self, struct cons_cache *cache, struct statistics *stat, int addr);
+	bool (*write_try)(struct cons_cache_controller *self, struct cons_cache *cache, struct statistics *stat, int addr);
+
+	// Variables for cache_controller
+	// About cache
+	int capacity;
+	int block_size;
+	int way_numbers;
+	int set_numbers;
+	
+	int tag_filter_bits;
+	int index_filter_bits;
+};
+
+void *cache_initializer(struct cache_config *config);
+
+bool cache_query(struct cons_cache_controller *cache_cont, struct cons_cache *cache, struct statistics *stat, int access_type, int addr);
+
+void cache_filler(struct cons_cache_controller *cache_cont, struct cons_cache *cache, struct statistics *stat, int addr);
